332de56f02b023d73603533feaa5316ec225d582
Miguel Batista <msb2@cesar.school>
2026-02-04 22:58:34 -0300

    updated backend structure

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000',
+        target: 'http://127.0.0.1:3000',
         changeOrigin: true,
       }
     }
